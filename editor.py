94c7485cbce7e91f07df35ce6997db0cbaa9523d
Peter-L-SVK <leukanic.peter@gmail.com>
2025-03-25 14:35:13 +0100

    - proper implementation of GPL v3

--- editor.py
+++ editor.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# PyLine 0.1 - Line editor (GPLv3)
+# Copyright (C) 2018 Peter Leukanič
+# License: GNU GPL v3+ <https://www.gnu.org/licenses/gpl-3.0.txt>
+# This is free software with NO WARRANTY.
+
 import sys
 import os
 import dirops
@@ -15,7 +20,7 @@ def main():
     dirops.default_path(original_destination)
     current_dir = dirops.currentdir()
     
-    print('PyLine 0.1 for Linux/BSD  Copyright (C) 2018  Peter Leukanič')
+    print('PyLine 0.1 - (GPLv3) for Linux/BSD  Copyright (C) 2018  Peter Leukanič')
     print('This program comes with ABSOLUTELY NO WARRANTY; for details type \'i\'.\n')
     
     try:
